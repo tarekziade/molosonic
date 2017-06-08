569110b55498b2aff0f90ed6324076fc634ff1c9
Tarek Ziade <tarek@ziade.org>
2017-06-08 10:13:22 +0200

    5 workers on the same etherpad lite

--- loadtest.py
+++ loadtest.py
@@ -1,5 +1,16 @@
+import asyncio
+from arsenic.errors import NoSuchElement, ArsenicTimeout, JavascriptError
+
 from molosonic import setup_browser, teardown_browser
 import molotov
+
+
+PAD = 'http://pad.mocotoolsprod.net/p/molosonic'
+
+
+@molotov.global_setup()
+def test_starts(args):
+    molotov.set_var('event', asyncio.Event())
 
 
 @molotov.setup_session()
@@ -12,15 +23,53 @@ async def _teardown_session(wid, session):
     await teardown_browser(session)
 
 
+_GET = """
+var text = require('ep_etherpad-lite/static/js/pad_editor').padeditor.ace.exportText();
+
+var div = document.getElementById('padText');
+if (!div) {
+    var div = document.createElement('div');
+    div.id = 'padText';
+    document.body.appendChild(div);
+}
+
+div.textContent = text;
+
+"""
+
+
 @molotov.scenario(1)
 async def example(session):
     firefox = session.browser
+    ev = molotov.get_var('event')
+    wid = session.worker_id
 
-    # go to ziade.org
-    await firefox.get('https://ziade.org')
+    if wid != 4:
+        # wait for worker 4 to edit the pad
+        await ev.wait()
 
-    # wait up to 5 seconds to get the h1 element from the page
-    h1 = await firefox.wait_for_element(5, 'h1')
+        # go to the pad
+        await firefox.get(PAD)
 
-    # print the text of the h1 element
-    print(await h1.get_text())
+        not_loaded = True
+
+        while not_loaded:
+            # wait for the pad to fill
+            try:
+                await firefox.execute_script(_GET)
+                el = await firefox.wait_for_element(5, '#padText')
+            except (NoSuchElement, ArsenicTimeout, JavascriptError):
+                asyncio.sleep(1.)
+                continue
+            else:
+                content = await el.get_text()
+                not_loaded = content == '(awaiting init)'
+                if not_loaded:
+                    asyncio.sleep(1.)
+        # content is the content of the pad we can check once it's
+        # edited by worker 9
+
+    else:
+        # worker 4 is adding content into the pad
+        # XXX edit pad
+        ev.set()
